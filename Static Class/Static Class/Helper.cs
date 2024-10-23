a003dad1d080703dd30b61122ba3cf6f516c4434
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-10-23 10:37:11 +0400

    Initial

---
+++ Static Class/Static Class/Helper.cs
@@ -0,0 +1,95 @@
+﻿namespace Static_Class
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int number)
+        {
+            if (number == 0)
+            {
+                return false;
+            }
+            else if (number % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEven(this int number)
+        {
+            if (number == 0)
+            {
+                return false;
+            }
+            else if (number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool HasDigit(this string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+            //foreach ilə yazılış
+            //foreach (char c in word)
+            //{
+            //    if (char.IsDigit(c))
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+        }
+
+        public static bool CheckPassword(this string password)
+        {
+            // Regex ilə yazılış
+            //return Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c))
+                {
+                    hasUpper = true;
+                }
+                else if (char.IsLower(c))
+                {
+                    hasLower = true;
+                }
+                else if (char.IsDigit(c))
+                {
+                    hasDigit = true;
+                }
+
+                if (hasUpper && hasLower && hasDigit)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string Capitalize(this string word)
+        {
+            return word.Trim().Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+        }
+    }
+}
---
+++ Static Class/Static Class/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Static_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n0. Exit program \n1. IsOdd() method \n2. IsEven() \n3. HasDigit() method \n4. CheckPassword() method \n5. Capitalize() method");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your number: ");
+                        int number = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Entered number is odd: " + number.IsOdd());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nEnter your number: ");
+                        int number2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Entered number is even: " + number2.IsEven());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nEnter word: ");
+                        string word = Console.ReadLine();
+                        Console.WriteLine("Entered word has digit: " + word.HasDigit());
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nEnter your password: ");
+                        string password = Console.ReadLine();
+                        Console.WriteLine("Your password is in correct format: " + password.CheckPassword());
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\nEnter your word: ");
+                        string word2 = Console.ReadLine();
+                        Console.WriteLine("Capitalized word: " + word2.Capitalize());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            while (choice != 0);
+        }
+    }
+}
